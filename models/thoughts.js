9dbd39761c5a4fc0bcf72e38a8a782a915b61915
Rick Johnson <carl4917@umn.edu>
2022-03-09 20:40:22 -0600

    adding though schema model and reaction shema to thought.js

---
+++ models/thoughts.js
@@ -0,0 +1,70 @@
+const { Schema, model, Types } = require("mongoose");
+const date = require("../utils/date");
+
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            validate: {
+                minlength: 1,
+                maxlength: 280
+            }
+        },
+
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (createdAtTime) => date(createdAtTime)
+        },
+
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [reactionsSchema]
+    },
+
+    {
+        toJSON:{
+            virtuals: true,
+            getters: true
+        }
+    }
+)
+
+
+const reactionsSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            validate: {
+                maxlength: 280
+            }
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (createdAtTime) => date(createdAtTime)
+        }
+    }
+);
+
+thoughtSchema.virtual("reactionNum").get(function() {
+    return this.reactions.length;
+});
+
+const Thoughts = model("thoughts", thoughtSchema);
+
+
+module.exports = Thoughts;
