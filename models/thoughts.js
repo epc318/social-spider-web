23431f3625b982378543a586e5fa09d45bda6bdd
Rick Johnson <carl4917@umn.edu>
2022-03-12 11:21:08 -0600

    more testing, got the app working!

--- models/thoughts.js
+++ models/thoughts.js
@@ -10,7 +10,6 @@ const reactionsSchema = new Schema(
         reactionBody: {
             type: String,
             required: true,
-            minlength: 1,
             maxlength: 280
         },
         username: {
@@ -31,10 +30,8 @@ const thoughtSchema = new Schema(
         thoughtText: {
             type: String,
             required: true,
-            validate: {
-                minlength: 1,
-                maxlength: 280
-            }
+            minlength: 1,
+            maxlength: 280
         },
 
         createdAt: {
