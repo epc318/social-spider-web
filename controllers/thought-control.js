2646600828f6c385c37a2155a8f17ea3d896b125
Rick Johnson <carl4917@umn.edu>
2022-03-11 19:08:43 -0600

    finishing thought controls

--- controllers/thought-control.js
+++ controllers/thought-control.js
@@ -3,7 +3,7 @@ const { Thoughts } = require("../models");
 const thoughtControllers = {
     getAllThoughts(req, res) {
         Thoughts.find({})
-            .then(dbUserData => res.json(dbUserData))
+            .then(dbThotData => res.json(dbThotData))
             .catch(err => {
                 console.log(err);
                 res.status(400).json(err);
@@ -11,9 +11,81 @@ const thoughtControllers = {
     },
 
 
+    getThoughtById({ parameters }, res) {
+        Thoughts.findOne({ _id: parameters.id })
+        .then(dbThotData => {
+            if(!dbThotData) {
+                res.status(404).json({ message: "Though ID not found, please check input and try again!" });
+                return;
+            }
+            res.json(dbThotData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    },
 
 
+    createThought({ body }, res) {
+        Thoughts.create(body)
+        .then(dbThotData => res.json(dbThotData))
+        .catch(err => res.status(400).json(err));
+    },
 
+
+    updateThought({ parameters, body }, res) {
+        Thoughts.findOneAndUpdate({ _id: parameters.id }, body, { new: true, runValidators: true })
+        .then(dbThotData => {
+            if(!dbThotData) {
+                res.status(404).json({ message: "Though ID not found, please check input and try again!"});
+                return;
+            }
+            res.json(dbThotData);
+        })
+        .catch(err => res.status(400).json(err));
+    },
+
+
+    deleteThought({ parameters }, res) {
+        Thoughts.findOneAndDelete({ _id: parameters.id })
+            .then(dbThotData => {
+                if(!dbThotData) {
+                    res.json({ message: "Though ID not found, please check input and try again!" });
+                    return;
+                }
+                res.json(dbThotData);
+            })
+            .catch(err => res.status(400).json(err));
+    },
+
+
+    createReaction({ parameters, body }, res) {
+        Thoughts.findOneAndUpdate(
+            { _id: parameters.thoughtId },
+            { $push: { reactions: body } },
+            { new: true, runValidators: true }
+        )
+            .then(dbThotData => {
+                if(!dbThotData) {
+                    res.json({ message: "Though ID not found, please check input and try again!" });
+                    return;
+                }
+                res.json(dbThotData);
+            })
+            .catch(err => res.status(400).json(err));
+    },
+
+
+    deleteReaction({ parameters }, res) {
+        Thoughts.findOneAndUpdate(
+            { _id: parameters.thoughtId },
+            { $pull: { reactions: { reactionId: parameters.reactionId} } },
+            { new: true }
+        )
+            .then(dbThotData => res.json(dbThotData))
+            .catch(err => res.json(err));
+    }
 };
 
 module.exports = thoughtControllers;
